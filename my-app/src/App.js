134ba6bfd7cec9cc0436e41710a1e68135fcb5d4
j0nnyh4ff <56747966+j0nnyh4ff@users.noreply.github.com>
2020-06-27 15:53:41 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
 import LandingPage from './LandingPage';
+import SignUpForm from './SignUpForm';
 import './App.css';
 
 function App() {
   return (
     <div className="wrapper">
-      <Nav loggedIn={false}/>
-      <LandingPage />
+      <SignUpForm />
     </div>
   );
 }
